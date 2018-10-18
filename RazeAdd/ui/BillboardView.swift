b68a88f3acb63b857f526d38825d7d5eaa3cee13
Punyugi2012 <58160216@go.buu.ac.th>
2018-10-18 18:20:17 +0700

    added billboard view controller

---
+++ RazeAdd/ui/BillboardView.swift
@@ -0,0 +1,9 @@
+//
+//  BillboardView.swift
+//  RazeAdd
+//
+//  Created by punyawee  on 18/10/2561 BE.
+//  Copyright © 2561 Punyugi. All rights reserved.
+//
+
+import Foundation
---
+++ RazeAdd/ui/BillboardViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BillboardViewController.swift
+//  RazeAdd
+//
+//  Created by punyawee  on 18/10/2561 BE.
+//  Copyright © 2561 Punyugi. All rights reserved.
+//
+
+import UIKit
+
+class BillboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
